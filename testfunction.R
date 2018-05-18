50b6ba30bd3c6ab37d257a9e7e8ef3a1ac10744a
Kaiya Provost <kprovost@KLP-AMNH.internal.amnh.org>
2018-05-18 11:51:40 -0400

    test
    test

---
+++ testfunction.R
@@ -0,0 +1,15 @@
+#' Echo
+#' 
+#' This function echos whatever you give it.
+#' 
+#' @param echo A word or sentence to echo
+#'
+#' @export
+#' @examples
+#'
+#' echo('This is a test')
+
+
+echo = function(echo){
+  return(echo)
+}
